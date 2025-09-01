329842f764e17feaec8076ed0d0d2c2a8dae534e
Samuel Jimenez <sajimenezr1@gmail.com>
2025-08-31 19:00:12 -0500

    first commit of hooks app

---
+++ hooks-app/src/HooksApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HooksApp = () => {
+    return (
+        <div className="bg-gradient">
+            <h1 className="text-3xl font-bold underline">Hooks App</h1>
+        </div>
+    )
+}
