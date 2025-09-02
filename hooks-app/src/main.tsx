f39053f5f90c7adac36ae01123603949d488e756
Samuel Jimenez <sajimenezr1@gmail.com>
2025-09-02 09:42:14 -0500

    useState

--- hooks-app/src/main.tsx
+++ hooks-app/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './HooksApp'
+import { TrafficLight } from './01-useState/TrafficLight'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp />
+    <TrafficLight />
+    {/* <HooksApp /> */}
   </StrictMode>,
 )
